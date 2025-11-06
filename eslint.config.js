4dbf8791e5a360b2e1e1479fbcac07d70822831d
qaspirian-lab <qa.spirian@gmail.com>
2025-11-06 08:44:39 -0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,27 @@
 import js from "@eslint/js";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default [
   js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
+      parser: tseslint.parser,
       ecmaVersion: 2022,
+      sourceType: "module",
       globals: {
         document: "readonly",
         window: "readonly",
         localStorage: "readonly",
       },
     },
-    plugins: { "react-hooks": reactHooks, "react-refresh": reactRefresh },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
     rules: {
       "no-unused-vars": "warn",
       "react-refresh/only-export-components": "warn",
